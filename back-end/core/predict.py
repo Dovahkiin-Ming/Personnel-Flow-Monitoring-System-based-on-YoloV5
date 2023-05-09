58327400088e2946ba594bb67b4398ef4fcd13da
Chenyh924 <1639724384@qq.com>
2023-05-09 23:55:41 +0800

    支持独立部署

--- back-end/core/predict.py
+++ back-end/core/predict.py
@@ -1,5 +1,10 @@
 import cv2
 
+#为方便打包，路径引用方式改为自动获取
+import os
+PROJECT_DIR = os.path.dirname(__file__)
+DRAW_DIR = os.path.join(PROJECT_DIR,"..\\tmp\\draw").replace('\\', '/')
+
 def predict(dataset, model, ext):
     global img_y
     x = dataset[0].replace('\\', '/')
@@ -8,5 +13,5 @@ def predict(dataset, model, ext):
     print(file_name)
     x = cv2.imread(x)
     img_y, image_info = model.detect(x)
-    cv2.imwrite('./tmp/draw/{}.{}'.format(file_name, ext), img_y)
+    cv2.imwrite(DRAW_DIR+'/{}.{}'.format(file_name, ext), img_y)
     return image_info
